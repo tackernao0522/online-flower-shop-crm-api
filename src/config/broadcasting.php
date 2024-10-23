691eb2e800c65c533fc8734da73329254f68665e
takaki <takaki55730317@gmail.com>
2024-10-23 21:25:53 +0900

    Pusherの設定を本番環境向けに修正
    - broadcasting.php で Pusher のホストを  に変更
    - 本番環境の .env.production において Pusher と WebSocket の設定を修正
    - SSLはALBで処理するため、WebSocket設定のSSLオプションを削除
    - ローカル環境の .env ファイルへの影響なし

--- src/config/broadcasting.php
+++ src/config/broadcasting.php
@@ -37,7 +37,7 @@ return [
             'app_id' => env('PUSHER_APP_ID'),
             'options' => [
                 'cluster' => env('PUSHER_APP_CLUSTER'),
-                'host' => env('PUSHER_HOST', '127.0.0.1'),
+                'host' => env('PUSHER_HOST', 'api.pusherapp.com'),
                 'port' => (int) env('PUSHER_PORT', 443),
                 'scheme' => env('PUSHER_SCHEME', 'https'),
                 'encrypted' => true,
